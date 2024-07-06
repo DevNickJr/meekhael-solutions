cac1610a27a125de4d7d297c9906b4b57abefa11
DevNickjr <nicholasduadei14@gmail.com>
2024-07-06 14:54:34 +0100

    about updates

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import { BiMenu } from 'react-icons/bi'
 import { MdOutlineClose } from 'react-icons/md'
-import Logo from "@/assets/logo.jpg"
+import Logo from "@/assets/meek.png"
 import Link from 'next/link'
 import Image from 'next/image'
 import Button from '../Button'
-import { useSession, signIn, signOut } from "next-auth/react"
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -14,36 +13,30 @@ const Header = () => {
     <div className='section flex items-center justify-between bg-white text-black shadow-md py-2 md:py-5 fixed top-0 left-0 z-30 w-full min-h-[50px]'>
     <header className='flex items-center justify-between w-full '>
         <Link href={"/"}>
-          <Image src={Logo} className='w-12 h-12 md:h-12' alt='' />
+          <Image src={Logo} className='w-24 h-12 md:h-12' alt='' />
         </Link>
         <nav className="items-center hidden gap-2 lg:flex lg:gap-4 whitespace-nowrap">
           <ul className='flex flex-col items-center gap-2 text-sm font-medium lg:flex-row text-dark-light lg:gap-4'>
             <li><Link href="/" className={`pb-1.5 px-1 font-medium`}>Home</Link></li>
             <li><Link href="/about-us" className={`pb-1.5 px-1 font-medium`}>About</Link></li>
-            {/* <li><Link href="/" className={`pb-1.5 px-1 font-medium`}>Scholarship</Link></li> */}
-            <li><Link href="/winners" className={`pb-1.5 px-1 font-medium`}>Winners</Link></li>
-            <li><Link href="/ambassadors" className={`pb-1.5 px-1 font-medium`}>Ambassadors</Link></li>
-            <li><Link href="/news" className={`pb-1.5 px-1 font-medium`}>News</Link></li>
-            <li><Link href="/scholarships" className={`pb-1.5 px-1 font-medium`}>Available Scholarship</Link></li>
-            <li><Link href="/contact-us" className={`pb-1.5 px-1 font-medium`}>Contact Us</Link></li>
-            <li><Link href="/faqs" className={`pb-1.5 px-1 font-medium`}>FAQs</Link></li>
             <li className='relative bg-white cursor-pointer group'>
-              <span className={`pb-1.5 px-1 font-medium`}>Login</span>
+              <span className={`pb-1.5 px-1 font-medium`}>Our Services</span>
               <div className='absolute left-0 flex-col hidden gap-2 bg-white shadow-md top-6 group-hover:flex'>
-                <a href={`https://box.reinsys.net/brilliant/`} className='py-2 border-b-2'>
+                <a href={`/services`} className='py-2 border-b-2'>
                   <span className={`py-2 pb-2.5 px-6 text-xs font-medium`}>
-                    User
+                    Service 1
                   </span>
                 </a>
-                <Link href={`/login`} className='pb-2'>
+                <Link href={`/services`} className='pb-2'>
                   <span className={`py-2 pb-2.5 px-6 text-xs font-medium`}>
-                    Admin
+                    Service 2
                   </span>
                 </Link>
               </div>
             </li>
+            {/* <li><Link href="/contact-us" className={`pb-1.5 px-1 font-medium`}>Contact Us</Link></li> */}
             {/* {!user ? 
-            <li><Link href="/login" className={`${pathname==="/login" && "text-green border-b-2"} pb-1.5 px-1 font-medium`}>Login</Link></li>
+            <li><Link href="/services" className={`${pathname==="/services" && "text-green border-b-2"} pb-1.5 px-1 font-medium`}>Login</Link></li>
             :
             <li>
               <div onClick={() => dispatch({type: "LOGOUT"})} className='px-1 font-medium cursor-pointer'>
@@ -52,11 +45,11 @@ const Header = () => {
             </li>
             } */}
           </ul>
-          <a href={`https://box.reinsys.net/brilliant/`}>
-              <Button className={`py-2 pb-2.5 px-6 text-xs font-medium text-white bg-primary rounded-full`}>
-                Sign Up
+          <Link href="/contact-us">
+              <Button className={`py-2 px-6 text-xs font-medium text-white bg-primary rounded-full`}>
+                Contact Us
               </Button>
-          </a>
+          </Link>
         </nav>
       </header>
       <div className={`lg:hidden shadow fixed top-0 right-0 w-5/6 min-h-screen h-screen bg-white text-black px-4  py-2 md:px-10 z-30 ${isOpen ? "translate-x-0" : "translate-x-full"} transition-all duration-300`}>
@@ -64,30 +57,24 @@ const Header = () => {
           <ul className='flex flex-col text-sm font-medium text-dark-light gap-7 md:gap-4 lg:gap-7'>
             <li><Link href="/" className={`pb-1.5 px-1 font-medium`}>Home</Link></li>
             <li><Link href="/about-us" className={`pb-1.5 px-1 font-medium`}>About</Link></li>
-            {/* <li><Link href="/" className={`pb-1.5 px-1 font-medium`}>Scholarship</Link></li> */}
-            <li><Link href="/winners" className={`pb-1.5 px-1 font-medium`}>Winners</Link></li>
-            <li><Link href="/ambassadors" className={`pb-1.5 px-1 font-medium`}>Ambassadors</Link></li>
-            <li><Link href="/news" className={`pb-1.5 px-1 font-medium`}>News</Link></li>
-            <li><Link href="/scholarships" className={`pb-1.5 px-1 font-medium`}>Available Scholarships</Link></li>
-            <li><Link href="/contact-us" className={`pb-1.5 px-1 font-medium`}>Contact Us</Link></li>
-            <li><Link href="/faqs" className={`pb-1.5 px-1 font-medium`}>FAQs</Link></li>
             <li className='relative bg-white cursor-pointer group'>
-              <span className={`pb-1.5 px-1 font-medium`}>Login</span>
+              <span className={`pb-1.5 px-1 font-medium`}>Our Services</span>
               <div className='absolute left-0 flex-col hidden gap-2 bg-white shadow-md top-6 group-hover:flex'>
-                <a href={`https://box.reinsys.net/brilliant/`} className='py-2 border-b-2'>
+                <a href={`/services`} className='py-2 border-b-2'>
                   <span className={`py-2 pb-2.5 px-6 text-xs font-medium`}>
-                    User
+                    Service 1
                   </span>
                 </a>
-                <Link href={`/login`} className='pb-2'>
+                <Link href={`/services`} className='pb-2'>
                   <span className={`py-2 pb-2.5 px-6 text-xs font-medium`}>
-                    Admin
+                    Service 2
                   </span>
                 </Link>
               </div>
             </li>
+            <li><Link href="/contact-us" className={`pb-1.5 px-1 font-medium`}>Contact Us</Link></li>
             {/* {!user ? 
-            <li><Link href="/login" className={`${pathname==="/login" && "text-green"} pb-1.5 px-1 font-medium`}>Login</Link></li>
+            <li><Link href="/services" className={`${pathname==="/services" && "text-green"} pb-1.5 px-1 font-medium`}>Login</Link></li>
               :
               <li>
                 <div onClick={() => dispatch({type: "LOGOUT"})} className='px-1 font-medium cursor-pointer'>
@@ -96,11 +83,16 @@ const Header = () => {
               </li>
             } */}
           </ul>
-          <a href={`https://box.reinsys.net/brilliant/`}>
+          <Link href="/contact-us">
+            <Button className={`py-2 px-6 text-xs font-medium text-white bg-primary rounded-full`}>
+              Contact Us
+            </Button>
+          </Link>
+          {/* <a href={`/services`}>
             <Button className={`py-2 pb-2.5 px-6 text-xs font-medium text-white bg-primary rounded-full`}>
               Sign Up
             </Button>
-          </a>
+          </a> */}
         </nav>
       </div>
       { isOpen ? 
--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,75 +1,97 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import About2 from '@/assets/about1.png'
 import About1 from '@/assets/about2.png'
 import dbConnect from '@/lib/dbConnection';
 import About from '@/models/AboutModel';
 
+
+const joinUsImages  = [
+  About2,
+  About1,
+] 
+
 const AboutUs = (props: any) => {
     const { about } = props
+    const [t, setTranslated] = useState<IPageContent | null>(null)
+
+  useEffect(() => {
+    setTranslated(AboutContent['en'])
+  }, [])
+
     // console.log({props})
   return (
     <div className='overflow-hidden'>
         <Header />
-        <section className='flex flex-col gap-3 text-white section top-section grad-to-right'>
-            <h1 className="mb-3 text-4xl font-extrabold text-white md:text-5xl font-argentinum">About Us</h1>
-            <span className='font-bold'>Home / About Us</span>
+        <section className='flex flex-col items-center justify-center gap-3 px-12 pt-40 text-center text-white bg-white py-28 lg:px-24 grad-to-right'>
+            <h1 className="mb-3 text-4xl font-extrabold text-white md:text-5xl font-argentinum">{t?.title || "About Meekhaael Solution"}</h1>
+            <p className='max-w-lg md:text-lg'>{t?.content || "Empowering Businesses Through Innovative Technology Solutions"}</p>
         </section>
-        <section className='section pt-12 pb-10 relative after:absolute after:translate-y-6 after:bottom-0 after:-left-[10%] after:w-[120%] after:h-16 after:rounded-[100%] after:bg-white'>
-            <h1 className="py-3 text-2xl font-bold text-primary font-argentinum">
-                The Brilliant Brain Scholarship Scheme
-            </h1>
+        <section className='px-12 py-20 text-black bg-white lg:px-24'>
+          <div data-aos="fade-in" className='flex flex-col items-center gap-3 mb-12 text-center'>
+            <h2 className='text-3xl font-semibold text-primary'>{t?.joinUs?.title || "Empowering Your Financial Journey"}</h2>
+            <p className='max-w-lg md:text-lg md:text-base'>{t?.joinUs?.content || "Join us on this journey towards financial growth and prosperity"}.</p>
+          </div>
+          <div className='grid gap-6 mb-8 md:gap-12 md:grid-cols-2'>
+            {
+              t?.joinUs?.subsections?.map((item, index) => (
+                <div key={index} data-aos="slide-up" className="flex flex-col w-full gap-3 p-6 rounded-md shadow-md">
+                  <div className="flex-[0_0_48px] relative">
+                    <Image src={joinUsImages[index]} alt={item.title} className='object-cover rounded-md aspect-video max-h-60' />
+                  </div>
+                  <span className='text-lg font-semibold text-black'>{item.title}</span>
+                  <span>{item.content}</span>
+                </div>
+              )
+            )}
+          </div>
         </section>
-        <section className="py-20 text-white section bg-primary">
-            <div className="flex flex-col gap-12 mb-20 md:flex-row">
-                <div className="relative flex-1 md:-top-20">
-                    <Image width={100} height={100} src={about?.first_section?.image || About1} alt="" className="w-full max-w-sm max-h-450px" />
-                </div>
-                <div className="flex flex-col flex-1 max-w-lg gap-3">
-                    {about?.first_section?.text?.split('\n').map((text: string, i: number) => (
-                        <p key={i}>{text}</p>
-                    ))}
-                </div>
+        <section className='px-12 py-20 text-white grad-to-right lg:px-24'>
+          <div data-aos="fade-in" className='flex flex-col items-center gap-3 mb-12 text-center'>
+            <h2 className='text-3xl font-semibold'>{t?.history?.title || "Our Trading Company In Numbers"}</h2>
+            <p className='max-w-lg md:text-lg md:text-base'>{t?.history?.content || "Trace the history of our company since its foundation in 2016"}</p>
+          </div>
+        </section>
+        <section className="py-12 text-center section md:py-20">
+          <div className="flex flex-col items-center max-w-2xl gap-4 mx-auto mb-12">
+            <h3 className="mb-3 text-3xl font-extrabold capitalize md:text-5xl">
+              Our Services
+            </h3>
+          </div>
+          <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <div className="bg-[#F9E9E8] w-full rounded-2xl p-8 py-12 flex flex-col items-center gap-3">
+              <Image src={About1} alt="" className="w-16 h-16" />
+              <h5 className="mb-3 text-2xl font-extrabold capitalize">Dynamic Web Apps</h5>
+              <p className="text-[#6D6D6D]">We develop dynamic web apps with enhanced security and strict SLA compliance for maintenance.</p>
             </div>
-            <div className="flex flex-col gap-12 md:flex-row">
-                <div className="flex flex-col flex-1 max-w-lg gap-3">
-                    {about?.second_section?.text?.split('\n').map((text: string, i: number) => (
-                        <p key={i}>{text}</p>
-                    ))}
-                </div>
-                <div className="flex justify-end flex-1">
-                    {/* <Image src={About2} alt="" className="w-full max-w-sm" /> */}
-                    <Image width={100} height={100} src={about?.second_section?.image || About2} alt="" className="w-full max-w-sm max-h-450px" />
-                </div>
+            <div className="bg-[#F9E9E8] w-full rounded-2xl p-8 py-12 flex flex-col items-center gap-3">
+              <Image src={About1} alt="" className="w-16 h-16" />
+              <h5 className="mb-3 text-2xl font-extrabold capitalize">Mobile App Solutions</h5>
+              <p className="text-[#6D6D6D]">Develop and maintain mobile apps to maximize uptime and enhance user experience</p>
             </div>
-        </section>
-        <section className="section flex flex-col gap-3 pt-20 pb-20 relative after:absolute after:top-0 after:-translate-y-6 after:-left-[10%] after:w-[120%] after:h-16 after:rounded-[100%] after:bg-white">
-            {about?.third_section?.split('\n').map((text: string, i: number) => (
-                        <p key={i}>{text}</p>
-                    ))}
-            {/* <p>
-                At Brilliant Brains, we are committed to empowering individuals through education and fostering a community of lifelong learners. With a passion for knowledge and a dedication to excellence, we strive to provide transformative educational experiences that shape lives and make a positive impact.
-            </p>
-            <p>
-                Our vision is to be a leading institution that inspires and equips individuals to reach their full potential. We believe in the power of education to transform lives, unlock opportunities, and create a better future for individuals and society as a whole.
-            </p>
-            <p>
-                Our mission is to provide accessible, high-quality education that nurtures intellectual curiosity, fosters critical thinking, and promotes personal and professional growth. We aim to create an inclusive learning environment that celebrates diversity, encourages collaboration, and prepares individuals to thrive in a rapidly changing world.
-            </p>   
-            <p>
-                At Brilliant Brains, we take a holistic approach to education, focusing not only on academic excellence but also on the development of well-rounded individuals. We prioritize practical, hands-on learning experiences that bridge the gap between theory and practice. Through innovative teaching methods, state-of-the-art resources, and a supportive community, we aim to inspire a love for learning and cultivate essential skills for success in the modern world.
-            </p>
-            <p>
-                Our team of passionate educators and staff members are the heart and soul of Brilliant Brains. With expertise in their respective fields, they are committed to providing a nurturing and stimulating learning environment. Our faculty members are not only knowledgeable professionals but also mentors who guide and inspire students to explore their passions and achieve their goals.
-            </p>    
-            <p>
-                We maintain high standards of academic rigor, continually updating our curriculum to align with industry demands and emerging trends. We foster a culture of continuous improvement, encouraging students and staff alike to strive for excellence in all aspects of their educational journey.
-            </p>
-            <p>
-            Whether you are a student, parent, educator, or lifelong learner, we invite you to join our vibrant learning community. Together, we can embark on a journey of discovery, growth, and lifelong learning. Explore our range of programs, engage with our passionate community, and unlock your potential at Brilliant Brains.
-            </p> */}
+            <div className="bg-[#F9E9E8] w-full rounded-2xl p-8 py-12 flex flex-col items-center gap-3">
+              <Image src={About1} alt="" className="w-16 h-16" />
+              <h5 className="mb-3 text-2xl font-extrabold capitalize">Tailored Business Solutions</h5>
+              <p className="text-[#6D6D6D]">Custom solutions to meet unique business needs, driving efficiency and transformation</p>
+            </div>
+            {/* <div className="bg-[#F9E9E8] w-full rounded-2xl p-8 py-12 flex flex-col items-center gap-3">
+              <Image src={About1} alt="" className="w-16 h-16" />
+              <h5 className="mb-3 text-2xl font-extrabold capitalize">Expertise in Content Creation</h5>
+              <p className="text-[#6D6D6D]">Specializing in impactful content writing and engaging digital communication strategies.</p>
+            </div>
+            <div className="bg-[#F9E9E8] w-full rounded-2xl p-8 py-12 flex flex-col items-center gap-3">
+              <Image src={About1} alt="" className="w-16 h-16" />
+              <h5 className="mb-3 text-2xl font-extrabold capitalize">Beyond Technology</h5>
+              <p className="text-[#6D6D6D]">Enhancing landscapes and promoting environmental beautification as part of our mission</p>
+            </div>
+            <div className="bg-[#F9E9E8] w-full rounded-2xl p-8 py-12 flex flex-col items-center gap-3">
+              <Image src={About1} alt="" className="w-16 h-16" />
+              <h5 className="mb-3 text-2xl font-extrabold capitalize">Effective Design Solutions</h5>
+              <p className="text-[#6D6D6D]">Crafting designs that resonate and drive engagement across digital platforms</p>
+            </div> */}
+          </div>
         </section>
         <Footer />
     </div>
@@ -103,3 +125,51 @@ export const getServerSideProps = async () => {
         }
     }
 }
+
+interface ITranslations {
+  en: IPageContent;
+}
+
+
+export interface IPageContent {
+  title: string;
+  content: string;
+  joinUs: Section;
+  history: Section;
+}
+
+interface Section {
+  title?: string;
+  content?: string;
+  subsections?: Subsection[];
+}
+interface Subsection {
+  title: string;
+  content: string;
+}
+
+export const AboutContent: ITranslations = {
+  en: {
+    title: "About Meekhaael Solution",
+    content: "Empowering Businesses Through Innovative Technology Solutions",
+    joinUs: {
+        title: "Explore Opportunities with Meekhaael Solutions",
+        content: "Join us on this journey to better solutions",
+        subsections: [
+          {
+            title: "Our Mission",
+            content: "At Meekhaael Solutions, our mission is to empower businesses with cutting-edge technology solutions that drive growth, efficiency, and transformation"
+          },
+          {
+            title: "Our Vision",
+            content: "At Meekhaael Solutions, our mission is to empower businesses with cutting-edge technology solutions that drive growth, efficiency, and transformation"
+          }
+        ]
+      },
+      history:       {
+        title: "Get in Touch",
+        // title: "Meekhaael Solutions specializes in:",
+        content: "Explore how Meekhaael Solutions can transform your business. Contact us today to discuss your project and discover the power of innovative technology solutions.",
+      }
+  },
+};
